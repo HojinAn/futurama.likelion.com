0cf1bc6fea1c291f060cc380ae3606cd3bead04b
HojinAn <asdv94@naver.com>
2021-12-26 14:25:25 +0900

    ✨ Add 6 Routers

---
+++ src/pages/charaters/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from "next";
+
+const CharactersPage: NextPage = () => {
+  return (
+    <div>
+      <h1>Characters</h1>
+    </div>
+  );
+};
+
+export default CharactersPage;
