aa372295dae75b8aab7df3d6de64fc34b6ff3b54
HojinAn <asdv94@naver.com>
2021-12-27 14:14:24 +0900

    💄 Add Navigation Style

--- src/components/layouts/Navigation.tsx
+++ src/components/layouts/Navigation.tsx
@@ -1,28 +1,52 @@
+import styled from "@emotion/styled";
 import Link from "next/link";
 import { ROUTES } from "../../constants";
 import { ROUTE } from "../../types/ROUTE";
 
 export const Navigation = () => {
   return (
-    <header>
-      <h1>
+    <Header>
+      <PageTitle>
         <Link href="/">
           <a>Futurama</a>
         </Link>
-      </h1>
+      </PageTitle>
       <nav>
-        <ul>
+        <NavList>
           {ROUTES.map((routeObject: ROUTE) => {
             return (
-              <li key={routeObject.ID}>
+              <ListItem key={routeObject.ID}>
                 <Link href={routeObject.PATH}>
                   <a>{routeObject.LABEL}</a>
                 </Link>
-              </li>
+              </ListItem>
             );
           })}
-        </ul>
+        </NavList>
       </nav>
-    </header>
+    </Header>
   );
 };
+
+const Header = styled.header`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  background-color: #2ac1bc;
+  color: #fff;
+  font-weight: bold;
+`;
+
+const PageTitle = styled.h1`
+  font-size: 2rem;
+  padding: 1rem;
+`;
+
+const NavList = styled.ul`
+  display: flex;
+  font-size: 1rem;
+`;
+
+const ListItem = styled.li`
+  margin: 1rem;
+`;
