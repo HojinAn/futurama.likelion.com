b2871e6aff470a5fc3bb92584d004dba2ddc0b75
HojinAn <asdv94@naver.com>
2021-12-28 17:15:18 +0900

    🚑 Episodes 링크 오류 수정

--- src/components/episodes/EpisodesCard.tsx
+++ src/components/episodes/EpisodesCard.tsx
@@ -3,41 +3,41 @@ import Link from "next/link";
 import { Episodes } from "../../types/Episodes";
 
 interface EpisodesProps {
-  episodesData: Episodes;
+    episodesData: Episodes;
 }
 
 export const EpisodesCard = ({ episodesData }: EpisodesProps) => {
-  const { number, title, writers, originalAirDate, desc, id } = episodesData;
+    const { number, title, writers, originalAirDate, desc, id } = episodesData;
 
-  return (
-    <EpisodesInfo>
-      <Link href="episodes/1">
-        <a>
-          <p>Ep. No.{number}</p>
-          <strong>{title}</strong>
-          <p>{writers}</p>
-          <p>{originalAirDate}</p>
-          <Description>{desc}</Description>
-        </a>
-      </Link>
-    </EpisodesInfo>
-  );
+    return (
+        <EpisodesInfo>
+            <article>
+                <a>
+                    <p>Ep. No.{number}</p>
+                    <strong>{title}</strong>
+                    <p>{writers}</p>
+                    <p>{originalAirDate}</p>
+                    <Description>{desc}</Description>
+                </a>
+            </article>
+        </EpisodesInfo>
+    );
 };
 
 const EpisodesInfo = styled.article`
-  display: flex;
-  flex-direction: column;
-  align-items: left;
-  justify-content: flex-end;
-  padding: 1em;
-  box-shadow: 1px 1px 5px rgba(0, 0, 0, 0.3);
-  border-radius: 1em;
+    display: flex;
+    flex-direction: column;
+    align-items: left;
+    justify-content: flex-end;
+    padding: 1em;
+    box-shadow: 1px 1px 5px rgba(0, 0, 0, 0.3);
+    border-radius: 1em;
 `;
 
 const Description = styled.p`
-  overflow: hidden;
-  display: -webkit-box;
-  -webkit-line-clamp: 2;
-  -webkit-box-orient: vertical;
-  padding-bottom: 3px;
+    overflow: hidden;
+    display: -webkit-box;
+    -webkit-line-clamp: 2;
+    -webkit-box-orient: vertical;
+    padding-bottom: 3px;
 `;
